50c3a8d94b1c9edd4d329684f5ca825cd8d23645
Erdem OLKUN <eolkun@protel.com.tr>
2017-02-01 09:28:02 +0300

    Using nokogiri to parse enpara

--- application.rb
+++ application.rb
@@ -103,7 +103,22 @@ class MyApplication < Sinatra::Base
   end
 
   get "/api/v1/test" do
-    response = HTTParty.get 'https://www.google.com.tr'
+    begin
+      response = HTTParty.get 'http://www.finansbank.enpara.com/doviz-kur-bilgileri/doviz-altin-kurlari.aspx'
+      doc = Nokogiri::HTML response
+      result = String.new
+      doc.css('#pnlContent span dl').each do |row|
+        if (row.css('dt').text)=="USD"
+          result+="#{row.css('dd').first.text.split(' ').first} : #{row.css('dd').last.text.split(' ').first}"
+        elsif (row.css('dt').text)=="EUR"
+          result+=" - #{row.css('dd').first.text.split(' ').first} : #{row.css('dd').last.text.split(' ').first}"
+        end 
+       
+    end
+    puts result 
+    rescue Exception => e
+      puts "Error Occured!!!"
+    end 
   end
   
   put "/post/:id/" do
